4c2c33e1bd1d9ee871aeaa673f001d4aad93640d
Bryce Tham <brycetham@gmail.com>
2014-11-06 16:22:02 -0800

    Began Add Patient Template

---
+++ MedLight/AddPatientList.h
@@ -0,0 +1,18 @@
+//
+//  AddPatientList.h
+//  MedLight
+//
+//  Created by Bryce Tham on 11/6/14.
+//  Copyright (c) 2014 TeamOne. All rights reserved.
+//
+
+#ifndef MedLight_AddPatientList_h
+#define MedLight_AddPatientList_h
+
+@interface AddPatientList (Workaround)
+
+-(IBAction)unwindToList: (UIStoryboardSegue *)segue;
+
+@end
+
+#endif
