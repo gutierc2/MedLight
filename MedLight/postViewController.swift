9618eb5afc387415c18bec2753467426616a88d3
johnatt2 <kaylab@dhcp-v014-212.mobile.uci.edu>
2014-11-12 13:17:02 -0800

    Added patient profile pic option
    postviewcontroller code implemented

---
+++ MedLight/postViewController.swift
@@ -0,0 +1,55 @@
+//
+//  postViewController.swift
+//  MedLight
+//
+//  Created by Kay Lab on 11/7/14.
+//  Copyright (c) 2014 TeamOne. All rights reserved.
+//
+
+import UIKit
+
+class postViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var imageToPost: UIImageView!
+    
+    @IBAction func chooseImage(sender: AnyObject) {
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        println("Image selected")
+        self.dismissViewControllerAnimated(true, completion:nil)
+        
+        imageToPost.image = image
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
