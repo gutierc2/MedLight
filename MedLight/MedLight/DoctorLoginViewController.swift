e19f9ac0a07cce1a06b580fd3731e3c7b8d7723c
Christopher Gutierrez <gutierc2@uci.edu>
2014-11-12 13:15:57 -0800

    Completed Login and Sign Up, Included Parse

---
+++ MedLight/MedLight/DoctorLoginViewController.swift
@@ -0,0 +1,123 @@
+//
+//  DoctorLoginViewController.swift
+//  MedLight
+//
+//  Created by Christopher Gutierrez on 11/11/14.
+//  Copyright (c) 2014 TeamOne. All rights reserved.
+//
+
+import UIKit
+
+class DoctorLoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    func displayAlert(title:String, error:String) {
+        
+        var alert = UIAlertController(title: title, message: error, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        email.resignFirstResponder()
+        password.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func doctorLoginPressed(sender: UIButton) {
+        
+        var error = ""
+        
+        if (email.text == "")
+        {
+            error = "Please enter your email address."
+        }
+        else if (password.text == "")
+        {
+            error = "Please enter your password."
+        }
+            
+        if (error != "")
+        {
+            displayAlert("Oh, no!", error: error)
+        }
+        else
+        {
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            PFUser.logInWithUsernameInBackground(email.text, password:password.text) {
+                (user: PFUser!, signupError: NSError!) -> Void in
+                
+                
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if signupError == nil {
+                    println("logged in")
+                    self.performSegueWithIdentifier("loginSuccessful", sender: UIButton())
+                    
+                } else {
+                    
+                    if let errorString = signupError.userInfo?["error"] as? NSString {
+                        
+                        error = errorString
+                        
+                    } else {
+                        
+                        error = "Please try again later."
+                        
+                    }
+                    
+                    self.displayAlert("Could Not Log In", error: error)
+                    
+                    
+                }
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
