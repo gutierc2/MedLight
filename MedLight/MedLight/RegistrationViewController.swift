a7105714485698795c21f3c79ffbc0bdc8a328b3
Christopher Gutierrez <gutierc2@uci.edu>
2014-11-12 21:36:05 -0800

    Added empty "patients" list to new doctors

--- MedLight/MedLight/RegistrationViewController.swift
+++ MedLight/MedLight/RegistrationViewController.swift
@@ -93,6 +93,8 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
             user["email"] = email.text
             user["phoneNumber"] = phoneNumber.text
             user["pagerNumber"] = pagerNumber.text
+            user["patients"] = []
+            
             
             user.signUpInBackgroundWithBlock
                 {
